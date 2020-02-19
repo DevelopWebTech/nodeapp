a4f7b81e4b0ac2c11a78ff381b5d8ef971baa27d
DevelopWebTech <50685439+DevelopWebTech@users.noreply.github.com>
2020-02-19 15:55:38 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,105 @@
+// server.js
+// where your node app starts
+
+// set up github passport for oauth
+require('dotenv').config()
+var passport = require('passport');
+var GithubStrategy = require('passport-github').Strategy;
+
+// the process.env values are set in .env
+passport.use(new GithubStrategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'https://'+process.env.PROJECT_DOMAIN+'.glitch.me/login/github/return',
+},
+function(token, tokenSecret, profile, cb) {
+  return cb(null, profile);
+}));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// init project
+var express = require('express');
+var app = express();
+var expressSession = require('express-session');
+
+// cookies are used to save authentication
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static('public'));
+app.use(expressSession({ secret:'observingboats', resave: true, saveUninitialized: true, maxAge: (90 * 24 * 3600000) }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// index route
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// on clicking "logoff" the cookie is cleared
+app.get('/logoff',
+  function(req, res) {
+    res.clearCookie('github-passport-example');
+    res.redirect('/');
+  }
+);
+
+app.get('/auth/github', passport.authenticate('github'));
+
+app.get('/login/github/return', 
+  passport.authenticate('github', 
+    { successRedirect: '/setcookie', failureRedirect: '/' }
+  )
+);
+
+// on successful auth, a cookie is set before redirecting
+// to the success view
+app.get('/setcookie', requireUser,
+  function(req, res) {
+    if(req.get('Referrer') && req.get('Referrer').indexOf(process.env.PROJECT_DOMAIN)!=-1){
+      res.cookie('github-passport-example', new Date());
+      res.redirect('/success');
+    } else {
+       res.redirect('/');
+    }
+  }
+);
+
+// if cookie exists, success. otherwise, user is redirected to index
+app.get('/success', requireLogin,
+  function(req, res) {
+    if(req.cookies['github-passport-example']) {
+      res.sendFile(__dirname + '/views/success.html');
+    } else {
+      res.redirect('/');
+    }
+  }
+);
+
+function requireLogin (req, res, next) {
+  if (!req.cookies['github-passport-example']) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+function requireUser (req, res, next) {
+  if (!req.user) {
+    res.redirect('/');
+  } else {
+    next();
+  }
+};
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
